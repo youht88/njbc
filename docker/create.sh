be98812bc2fc77ed84d764f38d50d3afbbc5c1dd
youht88 <youht@ylzinfo.com>
2019-12-27 02:29:09 +0000

    ipfs from imac2

--- docker/create.sh
+++ docker/create.sh
@@ -7,3 +7,7 @@ docker run -dit --name node3 -p 34043:4000 -p 33043:3000 --network overlay_mynet
 docker run -dit --name node4 -p 34044:4000 -p 33044:3000 --network overlay_mynet -v /Users/youht/opt/source/nodejs/njbc_old:/njbc njbc:v1
 
 docker run -dit --name node5 -p 34045:4000 -p 33045:3000 --network overlay_mynet -v /Users/youht/opt/source/nodejs/njbc_old:/njbc njbc:v1
+
+
+#4b
+docker run -dit --name node1 -p 34053:4000 -v /home/pi/opt/source/nodejs/njbc:/njbc njbc
